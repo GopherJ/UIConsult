f1ab8accfbcedfeb6492af443a95c55421e4a3d7
Cheng JIANG <cheng.jiang@outlook.fr>
2018-11-08 23:20:52 +0100

    [ADD]: config and build script

---
+++ config/webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path');
+
+const config = module.exports = {
+    plugins: []
+};
+
+// Set context to the root of the project
+config.context = path.resolve(__dirname, '..');
+
+// Entry
+config.entry = {
+    index: path.resolve(__dirname, 'src/bin/index')
+};
+
+// Target
+config.target = 'node';
+
+// Output
+config.output = {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].js'
+};
+
+// Resolver
+config.resolve = {
+    extensions: ['.js']
+};
+
+
+// module
+config.module = {
+    rules: [
+        {
+            test: /\.js$/,
+            use: 'babel-loader',
+            exclude: /node_modules|vendor/
+        }
+    ]
+};
---
+++ scripts/build.js
@@ -0,0 +1,35 @@
+const config = require('../config/webpack.config');
+const webpack = require('webpack');
+const { exec } = require('pkg');
+
+process.env.NODE_ENV = 'production';
+
+const ora = require('ora');
+const path = require('path');
+const chalk = require('chalk');
+
+
+const spinner = ora('building for production...');
+spinner.start();
+
+webpack(config, (err, stats) => {
+    spinner.stop();
+
+    if (err) throw err;
+    process.stdout.write(stats.toString({
+        colors: true,
+        modules: false,
+        children: false,
+        chunks: false,
+        chunkModules: false
+    }) + '\n\n');
+
+    if (stats.hasErrors()) {
+        console.log(chalk.red('  Build failed with errors.\n'));
+        process.exit(1);
+    }
+
+    console.log(chalk.cyan('  Build complete.\n'))
+});
+
+
