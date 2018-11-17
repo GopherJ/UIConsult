7b637acea3b32182de03706b97fd6f6514a3dc26
Cheng JIANG <cheng.jiang@outlook.fr>
2018-11-17 23:41:52 +0100

    [ADD]: automatic load subcommands

--- src/index.js
+++ src/index.js
@@ -1,5 +1,29 @@
-import program from 'caporal';
+import cli from 'caporal';
+import subcommands from './subcommands';
 
-program
-    .version('0.1.0')
-    .parse(process.argv);
+cli
+    .version('0.1.0');
+
+subcommands.forEach(subcommand => {
+    const command = cli
+        .command(subcommand.command.name, subcommand.command.description);
+
+    if (subcommand.alias) {
+        command.alias(subcommand.alias);
+    }
+
+    if (subcommand.argument) {
+        command.argument(subcommand.argument, subcommand.argument.description);
+    }
+
+    if (subcommand.options && subcommand.options.length > 0) {
+        subcommand.options.forEach(option => {
+            command.option(option.var, option.description, option.type, option.default);
+        });
+    }
+
+    command.action(subcommand.action);
+});
+
+cli.parse(process.argv);
+
