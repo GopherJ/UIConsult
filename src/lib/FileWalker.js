95fd41ceca507411e98f8a0b1102c5a93505351a
Cheng JIANG <cheng.jiang@outlook.fr>
2018-11-17 22:03:34 +0100

    [CHANGE]: to arrow

--- src/lib/FileWalker.js
+++ src/lib/FileWalker.js
@@ -1,8 +1,8 @@
 import walk from 'walkdir';
 import fs from 'fs';
 
-export default function(dir, cb) {
-    walk(dir, function (path, stat) {
+export default (dir, cb) => {
+    walk(dir, (path, stat) => {
         fs.readFile(path, 'utf8', (err, data) => {
             if (err) cb(err, path, stat, null);
             else cb(null, path, stat, data);
