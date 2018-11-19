90d2e6a1872a041fc24bbaf021d38a9905910282
Cheng JIANG <cheng.jiang@outlook.fr>
2018-11-19 17:11:18 +0100

    [FALLBACK]: to no babel

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,3 @@
-export const isString = s => typeof s === 'string';
-
-export const isEmptyString = s => s === '';
+exports.isString = s => typeof s === 'string';
+exports.isEmptyString = s => s === '';
+exports.isArrayAndHasLength = arr => Array.isArray && arr.length > 0;
