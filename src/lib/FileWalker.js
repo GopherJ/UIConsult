3c3d03f5f617892f0ceb606d348bbe8bf5513dad
Cheng JIANG <cheng.jiang@outlook.fr>
2018-11-21 10:28:53 +0100

    [SWITCH]: to walk

--- src/lib/FileWalker.js
+++ src/lib/FileWalker.js
@@ -1,4 +1,4 @@
-const walk = require('walkdir');
+const { walk } = require('walk');
 const fs = require('fs');
 
 /**
@@ -8,13 +8,17 @@ const fs = require('fs');
  * @param {function} end
  */
 module.exports = (dir, file, end) => {
-    const emitter = walk(dir);
+    const emitter = walk(dir, {});
 
-    emitter.on('file', (path, stat) => {
-        fs.readFile(path, 'utf8', (err, data) => {
-            if (err) file(err, path, stat, null);
-            else file(null, path, stat, data);
+    emitter.on('file', (root, stat, next) => {
+        fs.readFile(stat.name, 'utf8', (err, data) => {
+            if (err) file(err, root, stat, null), next();
+            else file(null, root, stat, data), next();
         });
+    });
+
+    emitter.on('errors', (root, nodeStatsArray, next) => {
+        next();
     });
 
     emitter.on('end', end);
