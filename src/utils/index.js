b22ee7e4598515525c5d8f092c30b27f570372fc
Cheng JIANG <cheng.jiang@outlook.fr>
2018-11-21 10:38:31 +0100

    [ADD]

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,6 @@
 exports.isString = s => typeof s === 'string';
 exports.isEmptyString = s => s === '';
+exports.isArray = arr => Array.isArray(arr);
 exports.isArrayAndHasLength = arr => Array.isArray && arr.length > 0;
+exports.isFunction = f => typeof f === 'function';
+exports.isAllFuntion = arr => isArray(arr) && arr.every(f => isFunction(f));
