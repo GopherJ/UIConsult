6c101973b82fa597e4ee502bb316d47b7384943d
Cheng JIANG <cheng.jiang@outlook.fr>
2018-11-21 18:43:54 +0100

    [REMOVE]: toString

--- src/lib/Email.js
+++ src/lib/Email.js
@@ -1,8 +1,6 @@
 /**
  * Single Email
  */
-const table = require('cli-table3');
-
 class Email {
     constructor(sender, receivers, ccreceivers, bccreceivers, subject, date, content) {
         this.sender = sender;
@@ -12,23 +10,6 @@ class Email {
         this.subject = subject;
         this.date = date;
         this.content = content;
-    }
-
-    toString() {
-        const tb = new table({
-            head: ['Subject', 'Date', 'Sender', 'Receivers', 'Content'],
-            colWidths: [25, 25, 25, 25, 200]
-        });
-
-        tb.push([
-            this.subject,
-            this.date,
-            this.sender,
-            this.receivers.join('\n'),
-            this.content
-        ]);
-
-        return tb.toString();
     }
 }
 
