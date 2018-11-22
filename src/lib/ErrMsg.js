9f7b8ba8bc2a95ef2e904bd5ff9ccf183bcd4212
Cheng JIANG <cheng.jiang@outlook.fr>
2018-11-22 10:06:34 +0100

    [ADD]: permission denied error

--- src/lib/ErrMsg.js
+++ src/lib/ErrMsg.js
@@ -5,7 +5,8 @@
 const ErrMsg = {
     OPTION_INVALID_FORMAT: (option) => `Error: format of "${option}" is invalid`,
     OPTION_OUT_OF_RANGE: (option) => `Error: "${option}" is out Of range`,
-    IO_FAILED_TO_READ: (path) => `Error: failed to read "${path}"`
+    IO_FAILED_TO_READ: (path) => `Error: failed to read "${path}"`,
+    IO_PERMISSION_DENIED: (path) => `Error: permission denied "${path}"`
 };
 
 module.exports = ErrMsg;
