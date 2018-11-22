bc6b348a2083ee387c7caddd48d48faafcbd7c40
Cheng JIANG <cheng.jiang@outlook.fr>
2018-11-22 13:38:10 +0100

    [ADD]: Table.js

---
+++ src/lib/Table.js
@@ -0,0 +1,64 @@
+const Table = require('cli-table3');
+const os = require('os');
+const { execSync } = require('child_process');
+
+const {
+    isAll,
+    isString
+} = require('../utils');
+
+/**
+ * Table
+ */
+class Table {
+    constructor(head) {
+        this.cols = 80;
+
+        this.updateCols();
+
+        if (isAll(head, isString)) {
+            this.tb = new Table({
+                head,
+                colWidths: [1/6, 1/6, 1/6, 1/6, 1/6].map(x => Math.floor(x * this.cols))
+            });
+        }
+    }
+
+    updateCols() {
+        switch(os.platform()) {
+            case 'darwin':
+            case 'linux':
+                this.cols = process.stdout.columns;
+                break;
+            case 'win32':
+                const rs = execSync('mode con | findStr Columns').toString('utf8');
+                const numMatches = rs.match(/(\d+)/);
+                if (numMatches !== null) this.cols = +numMatches.slice(1).pop();
+                break;
+        }
+
+        return this;
+    }
+
+    updateHead(head) {
+        if (isAll(head, isString)) {
+            this.updateCols();
+
+            this.tb = new Table({
+                head,
+                colWidths: [1/6, 1/6, 1/6, 1/6, 1/6].map(x => Math.floor(x * this.cols))
+            });
+        }
+
+        return this;
+    }
+
+    pushRow(row) {
+        if (isAll(row, isString)) this.tb.push(row);
+        return this;
+    }
+
+    toString() {
+        return this.tb.toString();
+    }
+}
