ffdb8b69dcafa460f8c489cf8426e3353f0be92e
--global <liece886@gmail.com>
2018-12-02 13:57:39 +0100

    Removing useless code in buzzydays.js

--- src/subcmds/buzzydays.js
+++ src/subcmds/buzzydays.js
@@ -1,4 +1,5 @@
 /**
+ * Spec 3 
  * @author Cherchour Liece
  */
 const cli = require('caporal');
@@ -36,17 +37,17 @@ const alias = 'bzd';
 
 const command = {
     name: 'buzzydays',
-    description: "Show an employee's exchanged emails' statistics of specific period"
+    description: "Displays the list of the 10 days selected and the number of emails sent (outside working hours) for these days."
 };
 
 const arguments = {
     dir: {
         var: '<dir>',
-        description: 'Directory where store emails'
+        description: 'Directory where emails is stored'
     }, 
     employee: {
         var: '<employee>',
-        description: "Employee's fullname"
+        description: "An employee's fullname"
     }
 };
 
@@ -97,30 +98,22 @@ const parseDate = (dateStr, isDateFrom) => {
             y
         ] = matches.slice(1).map(x => +x);
 
-        // e.g. '2018'
         if (!isNumber(m) && !isNumber(d)) {
             return new Date(y, 0);
-        // e.g '01/2018'
         } else if (!isNumber(m)) {
-            // e.g. '13/2018'
             if (!isInRange([1, 12], d)) {
                 return new Error(ErrMsg.OPTION_OUT_OF_RANGE(VAR));
             } else {
-            // e.g. '12/2018'
                 return new Date(y, d - 1, 0);
             }
-        // e.g. '1/1/2018'
         } else {
-            // e.g. '34/1/2018', '1/34/2018'
             if (!isInRange([1, lastDayOfMonth(m, y)], d) || !isInRange([1, 12], m)) {
                 return new Error(ErrMsg.OPTION_OUT_OF_RANGE(VAR));
             } else {
-                // e.g '1/1/2018'
                 return new Date(y, m - 1, d);
             }
         }
     } else {
-        // e.g. 'asdj'
         return new Error(ErrMsg.OPTION_INVALID_FORMAT(VAR));
     }
 };
@@ -139,46 +132,46 @@ const parseDate = (dateStr, isDateFrom) => {
  * @return  {Error|Boolean}
  */
 const checkDateInRange = (email, options) => {
-    // date strings in options 
+
     const { dateFrom, dateTo } = options;
     const { date } = email;
 
-    // no range specified
     if (isUndefined(dateFrom) && isUndefined(dateTo)) {
         return true;
-    // end date is specified
     } else if (isUndefined(dateTo)) {
         const rs = parseDate(dateFrom, true);
 
-        // parsing error
         if (rs instanceof Error) return rs;
-        // email's date is out of range
+
         if(rs > date) return false;
+
         return true;
-    // start date is specified
     } else if (isUndefined(dateFrom)) {
         const rs = parseDate(dateTo, false);
 
-        // parsing error
+
         if (rs instanceof Error) return rs;
-        // email's date is out of range
+
         if(rs < date) return false;
         return true;
     }
 
-    // start date and end date are all specified
+
     const rsFrom = parseDate(dateFrom, true);
     const rsTo = parseDate(dateTo, false);
 
-    // parsing date error
+
     if (rsFrom instanceof Error) return rsFrom;
     if (rsTo instanceof Error) return rsTo
 
-    // email's date is out of range
+
     if (rsFrom > date || rsTo < date) return false;
     return true;
 };
-
+/**
+ * Check if an email has been sent or received outside workings hours
+ * @param {*} email An employee's sent or received email
+ */
 const outsideWorkingHours = email => {
     const WorkingHours1 = "08:01";
     const WorkingHours2 = "21:59";
@@ -244,7 +237,7 @@ const checkEmployeeName = (email, args) => {
     const re = /^(\w+)(?: +)(\w+)$/;
     const matches = employee.match(re);
 
-    // e.g 'cheng jiang'
+
     if (!isNull(matches)) {
         const [
             firstName,
@@ -258,7 +251,7 @@ const checkEmployeeName = (email, args) => {
         else 
             return exchanged.NONE;
     } else {
-        // e.g. 'slkdjlsd'
+
         return new Error(ErrMsg.OPTION_INVALID_FORMAT(arguments.employee.var));
     }
 };
@@ -267,41 +260,33 @@ const action = (args, options, logger) => {
     // start the spinner
     const spinner = ora(InfoMsg.Loading).start();
 
-    // intialisation
-    let sent = 0;
-    let received = 0;
-
-    // create table, detect terminal's width and use the width and table head
-    // to init a correct table
     const tb = new Table([
         'Rank',
         'Days',
         'Sent Emails'
     ]);
 
-    // start to read file recursively
+
     FileWalker(args.dir, (err, absPath, data) => {
-        // failed to read a file
+
         if (err) return logger.error(chalk.red(ErrMsg.IO_FAILED_TO_READ(absPath)));
 
-        // email parser instance
         const emailParser = new EmailParser(data);        
-        // parse email and return an Email instance
+
         const email = emailParser.parseAndCreateEmail();
         
-        // check date, if there is an error then bubble up
         const rsDate = (checkDateInRange(email, options) && outsideWorkingHours(email));               
-        // check employee's name, if there is an error then bubble up
+
         const rsEmployee = checkEmployeeName(email, args);        
         
-        // error
+
         if (rsDate instanceof Error) 
-            // stop spinner, log error, exit process
+
             spinner.stop(), logger.error(chalk.red(rsDate.message)), process.exit(1);
-        else if (rsEmployee instanceof Error) 
-            // stop spinner, log error, exit process
+        else if (rsEmployee instanceof Error)
+        
             spinner.stop(), logger.error(chalk.red(rsEmployee.message)), process.exit(1);
-        // no error
+
         else if (rsDate) {
             if(rsEmployee === exchanged.SENT)
             {
@@ -329,12 +314,8 @@ const action = (args, options, logger) => {
             }
         }
     }, () => {
-        // file walker ends correctly
-        // stop spinner
         spinner.stop();
 
-        // add a table row, every item must be string otherwise if fails to add
-        // more info => Table.js
         buzzyDaysArrays.sort(sortDescending);
         let i = 0;
         while(i < buzzyDaysArrays.length && i < 10)
@@ -347,12 +328,10 @@ const action = (args, options, logger) => {
             i++;
         }
 
-        // print table
+
         process.stdout.write(tb.toString());
     }, path => {
-        // file walker ends with an error of permission
-        // it fails to read a directory
-        // stop spinner
+
         spinner.stop();
         logger.error(chalk.red(ErrMsg.IO_PERMISSION_DENIED(path)));
     });
