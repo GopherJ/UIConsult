4bcdf48e4cd608d1ed491f9eb4c3c6a1fe5086cd
Cheng JIANG <cheng.jiang@outlook.fr>
2018-12-05 15:30:46 +0100

    [ADD]: windows eof

--- tests/utilsFunctions.test.js
+++ tests/utilsFunctions.test.js
@@ -5,25 +5,26 @@ const {
     uniqueWords,
     isOutsideWorkingHours } = require('../src/utils/index');
 
-const txt = `Message-ID: <8229279.1075851877390.JavaMail.evans@thyme>
-Date: Tue, 11 Jan 2000 09:45:00 -0800 (PST)
-From: mike.riedel@enron.com
-To: rick.cates@enron.com, butch.russell@enron.com, leo.nichols@enron.com,
-    rick.loveless@enron.com, ron.harkrader@enron.com,
-    ron.beidelman@enron.com, larry.campbell@enron.com,
-    william.kendrick@enron.com
-Subject: Team Meeting Information
-Mime-Version: 1.0
-Content-Type: text/plain; charset=us-ascii
-Content-Transfer-Encoding: 7bit
-X-From: Mike Riedel
-X-To: Rick Cates, Butch Russell, Leo Nichols, Rick Loveless, Ron Harkrader, Ron Beidelman, Larry Campbell, William Kendrick
-X-cc:
-X-bcc:
-X-Folder: \\Larry_Campbell_Nov2001_1\\Notes Folders\\2000 goals and objectives
-X-Origin: CAMPBELL-L
-X-FileName: lcampbe.nsf
-
+const txt = `Message-ID: <8229279.1075851877390.JavaMail.evans@thyme>\r
+Date: Tue, 11 Jan 2000 09:45:00 -0800 (PST)\r
+From: mike.riedel@enron.com\r
+To: rick.cates@enron.com, butch.russell@enron.com, leo.nichols@enron.com,\r
+    rick.loveless@enron.com, ron.harkrader@enron.com,\r
+    ron.beidelman@enron.com, larry.campbell@enron.com,\r
+    william.kendrick@enron.com\r
+Subject: Team Meeting Information\r
+Mime-Version: 1.0\r
+Content-Type: text/plain; charset=us-ascii\r
+Content-Transfer-Encoding: 7bit\r
+X-From: Mike Riedel\r
+X-To: Rick Cates, Butch Russell, Leo Nichols, Rick Loveless, Ron Harkrader, Ron Beidelman, Larry Campbell, William Kendrick\r
+X-cc:\r
+X-bcc:\r
+X-Folder: \\Larry_Campbell_Nov2001_1\\Notes Folders\\2000 goals and objectives\r
+X-Origin: CAMPBELL-L\r
+X-FileName: lcampbe.nsf\r
+\r\n
+\r\n
 Rick, please please please see the attached files for the teams 2000 objectives`
 
 
@@ -37,6 +38,7 @@ describe('test function utils', () => {
         expect(uniqueWords(email.subject)).to.be.eqls(['Team','Meeting','Information']);
     });
     it('words' , () => {
+        console.log(words(email.content))
         expect(words(email.content)).to.be.eqls([ 'Rick', 'please','please','please','see','the','attached','files','for','the','teams','objectives' ]);
     });
 });
